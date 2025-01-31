1f8fafe850e1d7a63b3b95be693bdadc9e693fe5
Mahendra Gadila <mahendra.gadila@broadcom.com>
2025-01-31 22:18:25 +0530

    First web hello web app

---
+++ spring-security/src/main/java/com/mrcool/spring_security/MrCoolSoftApplication.java
@@ -0,0 +1,13 @@
+package com.mrcool.spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrCoolSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrCoolSoftApplication.class, args);
+	}
+
+}
