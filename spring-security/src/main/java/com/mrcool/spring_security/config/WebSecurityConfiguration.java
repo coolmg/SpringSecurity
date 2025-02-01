a073ab5272ad099524336face4ab5e497ab01f7b
Mahendra Gadila <mahendra.gadila@broadcom.com>
2025-02-01 15:27:39 +0530

    with multiple users

--- spring-security/src/main/java/com/mrcool/spring_security/config/WebSecurityConfiguration.java
+++ spring-security/src/main/java/com/mrcool/spring_security/config/WebSecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.mrcool.spring_security.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,11 +15,19 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class WebSecurityConfiguration {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
+        http.authorizeHttpRequests(requests -> requests
                 .requestMatchers("myAccount","myBalance","myLoans","myCards").authenticated()
                 .requestMatchers("contact","notices","error").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails admin = User.withUsername("admin").password("{noop}admin").authorities("admin").build();
+        UserDetails user = User.withUsername("user").password("{noop}user").authorities("read").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
