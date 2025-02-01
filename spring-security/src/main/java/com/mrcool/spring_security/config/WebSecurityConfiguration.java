1ea5950b93f033beb99d4156edf6f2541de87a25
Mahendra Gadila <mahendra.gadila@broadcom.com>
2025-02-01 15:39:29 +0530

    with password encoder

--- spring-security/src/main/java/com/mrcool/spring_security/config/WebSecurityConfiguration.java
+++ spring-security/src/main/java/com/mrcool/spring_security/config/WebSecurityConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -25,9 +28,14 @@ public class WebSecurityConfiguration {
 
     @Bean
     UserDetailsService userDetailsService() {
-        UserDetails admin = User.withUsername("admin").password("{noop}admin").authorities("admin").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$T8BnDhXCT7LdS9z.hGyKfOHiphbXmbFxkynYP75M6.2pXkrxSuEIm").authorities("admin").build();
         UserDetails user = User.withUsername("user").password("{noop}user").authorities("read").build();
 
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
